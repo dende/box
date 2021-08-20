ff306db7c4b2a262078378959c63cb408511d220
Christian Hartlage <c@dende.de>
2021-08-20 08:37:03 +0200

    enable latex texts

--- box.py
+++ box.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as plt
+
+figsize = (6.4, 4.8)
+markersize = 2
+dpi = 200
 
 config = {
     'secr. roGFP2-Orp1': ['WT', '#19', '#131', '#69'],
@@ -8,14 +13,23 @@ config = {
 
 df = pd.read_excel("Pp_roGFP2_Data_evaluation.xlsx", index_col=[0,1], sheet_name="RatiosLines")  # type: pd.DataFrame
 
+matplotlib.rc("text", usetex=True)
+matplotlib.rc("font", family="serif")
+matplotlib.rc("font", serif="Computer Modern Roman")
+matplotlib.rc("figure", autolayout=True)
 
 for sensor, lines in config.items():
+    figure = plt.figure(figsize=figsize, dpi=dpi)
+
     data = []
     for line in lines:
         data.append(df.loc[(sensor, line), "Ratio"].values)
 
-    fig = plt.figure(figsize=(10, 7))
+    ax = figure.subplots()
 
-    bp = plt.boxplot(data)
+    bp = ax.boxplot(data)
+    ax.set_xticklabels([s.replace("#", "\#") for s in lines])
 
-    plt.show()
+    ax.set_ylabel("ratio (405nm/488nm)")
+
+plt.show()
