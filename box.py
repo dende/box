c0172e63a9d665623f64d4f937d124d03c56c46c
Christian Hartlage <c@dende.de>
2021-08-23 22:04:41 +0200

    flaksfjljk

--- box.py
+++ box.py
@@ -1,35 +1,54 @@
+import numpy as np
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 
-figsize = (6.4, 4.8)
-markersize = 2
-dpi = 200
 
-config = {
-    'secr. roGFP2-Orp1': ['WT', '#19', '#131', '#69'],
-    'secr. Grx1-roGFP2iL': ['WT', '#159', '#278', '#222']
-}
+def set_box_color(bp, color):
+    plt.setp(bp['boxes'], color=color)
+    plt.setp(bp['whiskers'], color=color)
+    plt.setp(bp['caps'], color=color)
+    plt.setp(bp['medians'], color=color)
 
-df = pd.read_excel("Pp_roGFP2_Data_evaluation.xlsx", index_col=[0,1], sheet_name="RatiosLines")  # type: pd.DataFrame
 
-matplotlib.rc("text", usetex=True)
-matplotlib.rc("font", family="serif")
-matplotlib.rc("font", serif="Computer Modern Roman")
-matplotlib.rc("figure", autolayout=True)
+def main():
 
-for sensor, lines in config.items():
-    figure = plt.figure(figsize=figsize, dpi=dpi)
+    figsize = (6.4, 4.8)
+    markersize = 2
+    dpi = 200
 
-    data = []
-    for line in lines:
-        data.append(df.loc[(sensor, line), "Ratio"].values)
+    config = {
+        'secr. roGFP2-Orp1': ['WT', '#19', '#131', '#69'],
+        'secr. Grx1-roGFP2iL': ['WT', '#159', '#278', '#222']
+    }
 
-    ax = figure.subplots()
+    wavelengths = [420, 480]
 
-    bp = ax.boxplot(data)
-    ax.set_xticklabels([s.replace("#", "\#") for s in lines])
+    df = pd.read_excel("Pp_roGFP2_Data_evaluation.xlsx", index_col=[0,1], sheet_name="Radiant efficiency")  # type: pd.DataFrame
 
-    ax.set_ylabel("ratio (405nm/488nm)")
+    matplotlib.rc("text", usetex=True)
+    matplotlib.rc("font", family="serif")
+    matplotlib.rc("font", serif="Computer Modern Roman")
+    matplotlib.rc("figure", autolayout=True)
 
-plt.show()
+    for sensor, lines in config.items():
+
+        for wavelength in wavelengths:
+            data = []
+            figure = plt.figure(figsize=figsize, dpi=dpi)
+
+            ax = figure.subplots()
+
+            for line in lines:
+                data.append(df.loc[(sensor, line), f"Avg Radiant Efficiency  at {wavelength} nm"].values)
+
+            ax.boxplot(data)
+            ax.set_xticklabels([s.replace("#", "\#") for s in lines])
+            ax.set_ylabel("Radiant Efficiency [p/s/cm²/sr] / [µW/cm²]")
+            ax.set_title(f"Radiant Efficience of {sensor} lines at {wavelength} nm")
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
