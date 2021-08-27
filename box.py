320ee8183ab0edc93f4d6a4f75cc6d6548679b8c
Christian Hartlage <c@dende.de>
2021-08-27 22:11:37 +0200

    aksjflsakfj

--- box.py
+++ box.py
@@ -26,22 +26,25 @@ def main():
     #     'secr. Grx1-roGFP2iL': ['WT', '#159', '#278', '#222']
     # }
 
+    # config = {
+    #     'secrroGFP2-Orp1': ['#19', '#69', '#131'],
+    #     'secrGrx1-roGFP2iL': ['#159', '#222', '#278'],
+    # }
+
     config = {
-        'secrroGFP2-Orp1': ['#19', '#69', '#131'],
-        'secrGrx1-roGFP2iL': ['#159', '#222', '#278'],
+        'secr. roGFP2-Orp1': ["DPS", "phys", "DTT"]
     }
 
     wavelengths = [420, 480]
-    treatments = ["phys", "DPS", "DTT"]
 
-    df = pd.read_excel("roGFP2_stable_lines_ratios_calibration_CLSM.xlsx", index_col=[0, 1, 2], sheet_name="Tabelle1")  # type: pd.DataFrame
+    df = pd.read_excel("210507_Ratios_trans-expr_secr_roGFP2-Orp1.xlsx", index_col=[0, 1], sheet_name="Tabelle1")  # type: pd.DataFrame
 
     matplotlib.rc("text", usetex=True)
     matplotlib.rc("font", family="serif")
     matplotlib.rc("font", serif="Computer Modern Roman")
     matplotlib.rc("figure", autolayout=True)
 
-    for sensor, lines in config.items():
+    for sensor, treatments in config.items():
         figure = plt.figure(figsize=figsize, dpi=dpi)
 
         ax = figure.subplots()
@@ -50,26 +53,18 @@ def main():
         ax.plot([], c=blue, label='reduced')
         ax.legend()
 
-        line_basepos = 1
-        for line in lines:
-            data = []
+        data = []
+        bp = ax.boxplot(df.loc[(sensor, "DPS"), f"Ratio"].values, positions=[1], widths=.35)
+        set_box_color(bp, red)
+        bp = ax.boxplot(df.loc[(sensor, "phys."), f"Ratio"].values, positions=[2], widths=.35)
+        set_box_color(bp, black)
+        bp = ax.boxplot(df.loc[(sensor, "DTT"), f"Ratio"].values, positions=[3], widths=.35)
+        set_box_color(bp, blue)
 
-            bp = ax.boxplot(df.loc[(sensor, line, "DPS" ), f"Ratio"].values, widths=0.18, positions=[line_basepos - 0.2])
-            set_box_color(bp, red)
-            bp = ax.boxplot(df.loc[(sensor, line, "phys"), f"Ratio"].values, widths=0.18, positions=[line_basepos])
-            set_box_color(bp, black)
-            bp = ax.boxplot(df.loc[(sensor, line, "DTT" ), f"Ratio"].values, widths=0.18, positions=[line_basepos + 0.2])
-            set_box_color(bp, blue)
-
-            ax.boxplot(data)
-            ax.set_xticks([1,3,5])
-
-            ax.set_xticklabels([s.replace("#", r"\#") for s in lines])
-            ax.set_ylabel("Ratio of 405nm / 488nm")
-            ax.set_title(f"Ratios of {sensor}")
-            line_basepos = line_basepos + 2
+        ax.set_xticklabels(treatments)
+        ax.set_ylabel("Ratio of 405nm / 488nm")
+        ax.set_title(f"Ratios of {sensor}")
     plt.show()
-
 
 
 if __name__ == "__main__":
